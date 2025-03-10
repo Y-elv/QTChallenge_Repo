8d17fa181da5e78912e0ef5a02a116277e521647
mugishaelvis456@gmail.com <mugishaelvis456@gmail.com>
2025-03-10 20:57:04 +0200

    fix(backend):fixing some erros from docker deploy

--- service/src/main/java/com/example/urlShortening/UrlShorteningApplication.java
+++ service/src/main/java/com/example/urlShortening/UrlShorteningApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories("com.example.urlShortening.repositories")
 public class UrlShorteningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShorteningApplication.class, args);
